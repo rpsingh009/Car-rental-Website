e3425519ee2d64cb87f9b0ae43eac08c05436232
rpsingh009 <43109151+rpsingh009@users.noreply.github.com>
2021-05-31 15:05:01 +0530

    Update contact-us.php

--- carrental/contact-us.php
+++ carrental/contact-us.php
@@ -202,6 +202,4 @@ foreach($results as $result)
 <script src="assets/js/owl.carousel.min.js"></script>
 
 </body>
-
-<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/contact-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:26:55 GMT -->
 </html>
