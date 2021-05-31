a6239386965d909523b5b4ea230f9f1365808870
rpsingh009 <43109151+rpsingh009@users.noreply.github.com>
2021-05-31 15:04:16 +0530

    Update index.php

--- carrental/index.php
+++ carrental/index.php
@@ -232,5 +232,4 @@ foreach($results as $result)
 
 </body>
 
-<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
 </html>
